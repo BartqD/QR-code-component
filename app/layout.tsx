e428424926250e9eb5815f715ea7ee07f9f76ac7
Bartosz <bdrozdek@gmail.com>
2024-03-20 09:49:06 +0100

    style, docs: change font and create README file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Outfit } from 'next/font/google'
 
 export const metadata: Metadata = {
 	title: 'QR Code',
-	description: 'QR code to scan', 
+	description: 'QR code to scan',
 }
+
+const outfit = Outfit({ weight: ['400', '700'], subsets: ['latin'] })
 
 export default function RootLayout({
 	children,
@@ -13,7 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body>{children}</body>
+			<body className={outfit.className}>{children}</body>
 		</html>
 	)
 }
