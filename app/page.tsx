a05f63f1427f02cc1ff8e67d6c58d2ef8b3c6528
Bartosz <bdrozdek@gmail.com>
2024-03-20 11:42:35 +0100

    style:change margins

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 							<Image src={qrCode} alt='QR code' width={288} height={288}></Image>
 						</div>
 						<div className={classes['card__content']}>
-							<h2 className={classes['card__content-title']}>Improve your front-end skills by building projects</h2>
+							<h1 className={classes['card__content-title']}>Improve your front-end skills by building projects</h1>
 							<p className={classes['card__content-text']}>
 								Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
 							</p>
@@ -23,7 +23,7 @@ export default function Home() {
 			</main>
 			<footer>
 				<div className={classes.attribution}>
-					Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>Frontend Mentor</a>. Coded by <a href=''>Bartosz</a>.
+					Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>Frontend Mentor</a>. Coded by <a href='https://github.com/BartqD'>Bartosz</a>.
 				</div>
 			</footer>
 		</Fragment>
